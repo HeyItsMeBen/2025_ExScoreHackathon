377786748bb1568aef0dd7ce0ae0aae84b38973f
HeyItsMeBen <111411605+HeyItsMeBen@users.noreply.github.com>
2025-02-21 15:39:42 -0800

    Still working on the cards

--- JavaScript/userData.js
+++ JavaScript/userData.js
@@ -15,3 +15,12 @@ console.log(sprite_gender);
 //Cards Collected
 
 let cardsFound = 0;
+let Creation_National_Assembly = false;
+let Estates_General = false;
+let French_Envelopment = false;
+let Storm_Bastille = false;
+let Tennis_Court_Oath = false;
+let First_Second_Estate = false;
+let King_Louis = false;
+let National_Assembly = false;
+let Third_Estate = false;
