9d2e15aa60226e2bba16a0cfcf8fae9e53edef0b
HeyItsMeBen <111411605+HeyItsMeBen@users.noreply.github.com>
2025-02-21 13:38:59 -0800

    Alright got the gender stuff added in

--- JavaScript/Resource.js
+++ JavaScript/Resource.js
@@ -1,36 +1,5 @@
 import { sprite_gender } from './userData.js';
 
-// class Resources {
-//   constructor() {
-//     // if (sprite_gender == "female") {
-//     //   this.toLoad = {
-//     //     male_sprite: "assests/female_character.png" //Switches to female
-//     //   };
-//     // } else {
-//     //   this.toLoad = {
-//     //     male_sprite: "assests/male_character.png" //Switches to male (default)
-//     //   };
-//     // }
-//     this.toLoad = {
-//       male_sprite: "assests/male_character.png", //Switches to male (default)
-//       path: "assests/path.png"
-//     };
-
-//     Object.keys(this.toLoad).forEach((key) => {
-//         const img = new Image();
-//         img.src = this.toLoad[key];
-//         this.images[key] = {
-//             image:img,
-//             isLoaded:false
-//         }
-//         img.onload = () => {
-//             this.images[key].isLoaded = true;
-//         }
-//     })
-//   }
-// }
-
-// export const resources = new Resources();
 class Resources {
   constructor() {
     if (sprite_gender == "female") {
@@ -45,10 +14,6 @@ class Resources {
         male_sprite: "assests/male_character.png"
       }
     }
-    // this.toLoad = {
-    //     path: "assests/path.png",
-    //     male_sprite: "assests/male_character.png"
-    // };
     this.images = {};
 
     Object.keys(this.toLoad).forEach((key) => {
