3104037d665b37de44cdbe46ddfee6aa7f402379
HeyItsMeBen <111411605+HeyItsMeBen@users.noreply.github.com>
2025-02-21 20:53:32 -0800

    Pushing Speech Bubbles

--- JavaScript/Levels.js
+++ JavaScript/Levels.js
@@ -6,27 +6,50 @@ import {FrameIndexPattern} from "./FrameIndexPattern.js";
 import {STAND} from "./NPCAnimations.js";
 
 export const scenes = {
-    1:{
+    1: {
         name: "paris_slums",
         background: resources.images.paris_slums,
         level: 1,
         y_bar: 300,
         characters: [
-{
+            {
                 sprite: new Sprite({
                     resource: resources.images.proletariat,
-                    frameSize: new Vector2(250,250),
+                    frameSize: new Vector2(250, 250),
                     hFrames: 2,
-                    vFrames:2,
-                    frame:0,
+                    vFrames: 2,
+                    frame: 0,
                     animations: new Animations({
                         stand: new FrameIndexPattern(STAND),
                     })
-                }), 
-                position: new Vector2(600,400)
+                }),
+                position: new Vector2(600, 400)
             }
         ],
         path: "assests/paris_slums.png",
-        
+    },
+
+    2: {
+        name: "path",
+        background: resources.images.path,  // Ensure this resource exists
+        level: 1,
+        y_bar: 400,
+        characters: [
+            {
+                sprite: new Sprite({
+                    resource: resources.images.proletariat,
+                    frameSize: new Vector2(250, 250),
+                    hFrames: 2,
+                    vFrames: 2,
+                    frame: 0,
+                    animations: new Animations({
+                        stand: new FrameIndexPattern(STAND),
+                    })
+                }),
+                position: new Vector2(600, 500)
+            }
+        ],
+        path: "assests/path.png",
     }
-}
+};
+
